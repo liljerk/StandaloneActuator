5c748517358b566a6d9a0b9c5742f26922ad5d44
laod <houshja@gmail.com>
2014-06-11 22:28:30 -0500

    Update to current src.

--- src/main/scala/example.scala
+++ src/main/scala/example.scala
@@ -4,6 +4,7 @@ import cpw.mods.fml.common
 import cpw.mods.fml.common.event.FMLPreInitializationEvent
 import cpw.mods.fml.common.event.FMLInitializationEvent
 import cpw.mods.fml.common.event.FMLPostInitializationEvent
+import cpw.mods.fml.common.FMLLog
 import cpw.mods.fml.common.Mod
 import cpw.mods.fml.common.Mod.EventHandler
 import cpw.mods.fml.common.registry.GameRegistry
@@ -12,25 +13,44 @@ import org.apache.logging.log4j.Logger
 
 import net.minecraft.block.BlockStone
 import net.minecraft.block.Block
+import net.minecraft.block.ITileEntityProvider
 import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.item.ItemStack
+import net.minecraft.tileentity
+import net.minecraft.tileentity.TileEntity
+import net.minecraft.world.World
+import net.minecraftforge.common.util.FakePlayerFactory
 
-class ActuatorBlock extends BlockStone{
+class MyTE extends TileEntity{
+
+  override def updateEntity{
+  }
+}
+
+class ActuatorBlock extends BlockStone with ITileEntityProvider{
 	setBlockName("doi")
 	setCreativeTab(CreativeTabs.tabBlock)
+
+  def createNewTileEntity(w: World, i: Int) : TileEntity = {
+    FMLLog.fine("Is something happening?")
+    return new MyTE
+  }
 }
 
 @Mod(modid = "ExampleMod", name = "ExampleMod", version = "0.0.0", modLanguage = "scala")
 object ExampleMod {
 	var log: Logger = null
+  var instance = this
 
 	@EventHandler
 	def preInit(e: FMLPreInitializationEvent) {
+    FMLLog.fine("Is something happening?")
 		log = e.getModLog
 	}
 
 	@EventHandler
 	def init(e: FMLInitializationEvent) {
+    FMLLog.fine("Is something happening?")
 		log.info("testing one two")
 		val b = new ActuatorBlock
 		GameRegistry.registerBlock(b, "doi")
@@ -39,5 +59,6 @@ object ExampleMod {
 
 	@EventHandler
 	def postInit(e: FMLPostInitializationEvent) {
+    FMLLog.fine("Is something happening?")
 	}
 }
